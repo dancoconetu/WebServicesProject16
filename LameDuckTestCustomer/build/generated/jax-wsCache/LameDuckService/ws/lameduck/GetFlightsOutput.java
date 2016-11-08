67bc773348e43b0d04d7f275c53af3f31cdd99e6
Dan <dancoconetu@gmail.com>
2016-11-08 21:34:54 +0100

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 804c2915abeaeee6cbc8fe5d275cc9210e508e87.

---
+++ LameDuckTestCustomer/build/generated/jax-wsCache/LameDuckService/ws/lameduck/GetFlightsOutput.java
@@ -0,0 +1,67 @@
+
+package ws.lameduck;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for getFlightsOutput complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="getFlightsOutput">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="flightInformation" type="{http://LameDuck.ws}flightInformation" maxOccurs="unbounded" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "getFlightsOutput", propOrder = {
+    "flightInformation"
+})
+public class GetFlightsOutput {
+
+    protected List<FlightInformation> flightInformation;
+
+    /**
+     * Gets the value of the flightInformation property.
+     * 
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the flightInformation property.
+     * 
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getFlightInformation().add(newItem);
+     * </pre>
+     * 
+     * 
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link FlightInformation }
+     * 
+     * 
+     */
+    public List<FlightInformation> getFlightInformation() {
+        if (flightInformation == null) {
+            flightInformation = new ArrayList<FlightInformation>();
+        }
+        return this.flightInformation;
+    }
+
+}
