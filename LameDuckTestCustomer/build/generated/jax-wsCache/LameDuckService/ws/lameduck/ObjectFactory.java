a5f1e50f7d6d050906a5b9e7038f49c407d06f25
jocius <justuasas@gmail.com>
2016-11-09 16:22:37 +0100

    update of lameduckcust

--- LameDuckTestCustomer/build/generated/jax-wsCache/LameDuckService/ws/lameduck/ObjectFactory.java
+++ LameDuckTestCustomer/build/generated/jax-wsCache/LameDuckService/ws/lameduck/ObjectFactory.java
@@ -24,8 +24,14 @@ import javax.xml.namespace.QName;
 @XmlRegistry
 public class ObjectFactory {
 
-    private final static QName _GetFlighsInput_QNAME = new QName("http://LameDuck.ws", "getFlighsInput");
-    private final static QName _GetFlightsOutput_QNAME = new QName("http://LameDuck.ws", "getFlightsOutput");
+    private final static QName _GetFlighsInputElement_QNAME = new QName("http://LameDuck.ws", "getFlighsInputElement");
+    private final static QName _BookFlightFaultElement_QNAME = new QName("http://LameDuck.ws", "bookFlightFaultElement");
+    private final static QName _CancelFlightInputElement_QNAME = new QName("http://LameDuck.ws", "cancelFlightInputElement");
+    private final static QName _BookFlightOutputElement_QNAME = new QName("http://LameDuck.ws", "bookFlightOutputElement");
+    private final static QName _GetFlightsOutputElement_QNAME = new QName("http://LameDuck.ws", "getFlightsOutputElement");
+    private final static QName _CancelFlightOutputElement_QNAME = new QName("http://LameDuck.ws", "cancelFlightOutputElement");
+    private final static QName _BookFlightInputElement_QNAME = new QName("http://LameDuck.ws", "bookFlightInputElement");
+    private final static QName _CancelFlightFaultElement_QNAME = new QName("http://LameDuck.ws", "cancelFlightFaultElement");
 
     /**
      * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: ws.lameduck
@@ -35,11 +41,11 @@ public class ObjectFactory {
     }
 
     /**
-     * Create an instance of {@link GetFlightsOutput }
+     * Create an instance of {@link CancelFlightInput }
      * 
      */
-    public GetFlightsOutput createGetFlightsOutput() {
-        return new GetFlightsOutput();
+    public CancelFlightInput createCancelFlightInput() {
+        return new CancelFlightInput();
     }
 
     /**
@@ -48,6 +54,22 @@ public class ObjectFactory {
      */
     public GetFlightsInput createGetFlightsInput() {
         return new GetFlightsInput();
+    }
+
+    /**
+     * Create an instance of {@link BookFlightInput }
+     * 
+     */
+    public BookFlightInput createBookFlightInput() {
+        return new BookFlightInput();
+    }
+
+    /**
+     * Create an instance of {@link GetFlightsOutput }
+     * 
+     */
+    public GetFlightsOutput createGetFlightsOutput() {
+        return new GetFlightsOutput();
     }
 
     /**
@@ -67,21 +89,91 @@ public class ObjectFactory {
     }
 
     /**
+     * Create an instance of {@link CreditCardDetails }
+     * 
+     */
+    public CreditCardDetails createCreditCardDetails() {
+        return new CreditCardDetails();
+    }
+
+    /**
+     * Create an instance of {@link ExpirationDate }
+     * 
+     */
+    public ExpirationDate createExpirationDate() {
+        return new ExpirationDate();
+    }
+
+    /**
      * Create an instance of {@link JAXBElement }{@code <}{@link GetFlightsInput }{@code >}}
      * 
      */
-    @XmlElementDecl(namespace = "http://LameDuck.ws", name = "getFlighsInput")
-    public JAXBElement<GetFlightsInput> createGetFlighsInput(GetFlightsInput value) {
-        return new JAXBElement<GetFlightsInput>(_GetFlighsInput_QNAME, GetFlightsInput.class, null, value);
+    @XmlElementDecl(namespace = "http://LameDuck.ws", name = "getFlighsInputElement")
+    public JAXBElement<GetFlightsInput> createGetFlighsInputElement(GetFlightsInput value) {
+        return new JAXBElement<GetFlightsInput>(_GetFlighsInputElement_QNAME, GetFlightsInput.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link String }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://LameDuck.ws", name = "bookFlightFaultElement")
+    public JAXBElement<String> createBookFlightFaultElement(String value) {
+        return new JAXBElement<String>(_BookFlightFaultElement_QNAME, String.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link CancelFlightInput }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://LameDuck.ws", name = "cancelFlightInputElement")
+    public JAXBElement<CancelFlightInput> createCancelFlightInputElement(CancelFlightInput value) {
+        return new JAXBElement<CancelFlightInput>(_CancelFlightInputElement_QNAME, CancelFlightInput.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link Boolean }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://LameDuck.ws", name = "bookFlightOutputElement")
+    public JAXBElement<Boolean> createBookFlightOutputElement(Boolean value) {
+        return new JAXBElement<Boolean>(_BookFlightOutputElement_QNAME, Boolean.class, null, value);
     }
 
     /**
      * Create an instance of {@link JAXBElement }{@code <}{@link GetFlightsOutput }{@code >}}
      * 
      */
-    @XmlElementDecl(namespace = "http://LameDuck.ws", name = "getFlightsOutput")
-    public JAXBElement<GetFlightsOutput> createGetFlightsOutput(GetFlightsOutput value) {
-        return new JAXBElement<GetFlightsOutput>(_GetFlightsOutput_QNAME, GetFlightsOutput.class, null, value);
+    @XmlElementDecl(namespace = "http://LameDuck.ws", name = "getFlightsOutputElement")
+    public JAXBElement<GetFlightsOutput> createGetFlightsOutputElement(GetFlightsOutput value) {
+        return new JAXBElement<GetFlightsOutput>(_GetFlightsOutputElement_QNAME, GetFlightsOutput.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link Boolean }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://LameDuck.ws", name = "cancelFlightOutputElement")
+    public JAXBElement<Boolean> createCancelFlightOutputElement(Boolean value) {
+        return new JAXBElement<Boolean>(_CancelFlightOutputElement_QNAME, Boolean.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link BookFlightInput }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://LameDuck.ws", name = "bookFlightInputElement")
+    public JAXBElement<BookFlightInput> createBookFlightInputElement(BookFlightInput value) {
+        return new JAXBElement<BookFlightInput>(_BookFlightInputElement_QNAME, BookFlightInput.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link String }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://LameDuck.ws", name = "cancelFlightFaultElement")
+    public JAXBElement<String> createCancelFlightFaultElement(String value) {
+        return new JAXBElement<String>(_CancelFlightFaultElement_QNAME, String.class, null, value);
     }
 
 }
